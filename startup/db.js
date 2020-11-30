25a96afdc8dd4510aaad2eebe3d32c4229cf6ba1
towfiqul-islam <towfiqu@outlook.com>
2020-11-30 10:51:29 +0600

    added heroku database

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: '3307',
-  user: 'root',
-  password: 'root',
-  database: 'yos_db',
+  // host: 'localhost',
+  // port: '3307',
+  // user: 'root',
+  // password: 'root',
+  // database: 'yos_db',
+  host: 'aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'js1waekug7f7om05',
+  password: 'hd5chcblm23s2taw',
+  database: 'ckvksp61e4gyr6j3',
 });
 
 module.exports = connection;

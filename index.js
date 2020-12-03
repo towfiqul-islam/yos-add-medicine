595b2b6bbb5a3441b8b52f4ff71a7ced2fc4cef4
towfiqul-islam <towfiqu@outlook.com>
2020-12-03 10:23:49 +0600

    local server port updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
